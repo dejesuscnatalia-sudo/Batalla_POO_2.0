5771e210b7ef31d8812dc0c7b5f9f1261bb9f8b6
Markbenalt <gabzuniga07@gmail.com>
2026-03-25 15:30:31 -0700

    Refactor Menu class and fix formatting issues

--- Batalla_POO/Menu.cs
+++ Batalla_POO/Menu.cs
@@ -1,70 +1,74 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Batalla_POO
 {
     internal class Menu
-{
-    public int MakeMenu(string titulo, string[] opciones, int seleccionDefault = -1)
     {
-        Console.CursorVisible = false;
-        if (Console.CursorTop != 0)
+        public int MakeMenu(string titulo, string[] opciones, int seleccionDefault = -1)
         {
-            Console.WriteLine();
+            Console.CursorVisible = false;
+            if (Console.CursorTop != 0)
+            {
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(titulo + (seleccionDefault != -1 ? $"(Por defecto {seleccionDefault})" : "") + ":");
+            seleccionDefault = seleccionDefault == -1 ? 0 : seleccionDefault - 1;
+
+            int y = Console.CursorTop;
+            int cont = 0;
+            int cursorInicio = Console.CursorTop;
+
+            ConsoleKeyInfo accionKey = new ConsoleKeyInfo();
+            do
+            {
+                Console.CursorTop = cursorInicio;
+                cont = 0;
+
+                if (accionKey.Key == ConsoleKey.UpArrow)
+                {
+                    seleccionDefault--;
+                }
+                else if (accionKey.Key == ConsoleKey.DownArrow)
+                {
+                    seleccionDefault++;
+                }
+
+                if (seleccionDefault > opciones.Length - 1)
+                {
+                    seleccionDefault = 0;
+                }
+                else if (seleccionDefault < 0)
+                {
+                    seleccionDefault = opciones.Length - 1;
+                }
+
+                Array.ForEach(opciones, opcion =>
+                {
+                    if (cont == seleccionDefault)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.WriteLine($"-> {opcion}");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine("   " + opcion);
+                    }
+                    cont++;
+                });
+            } while ((accionKey = Console.ReadKey(true)).Key != ConsoleKey.Enter);
+
+            Console.Clear();
+            Console.WriteLine($"Operacion seleccionada: {opciones[seleccionDefault]}");
+            seleccionDefault++;
+            return seleccionDefault++;
         }
-
-        Console.WriteLine(titulo + (seleccionDefault != -1 ? $"(Por defecto {seleccionDefault})" : "") + ":");
-        seleccionDefault = seleccionDefault == -1 ? 0 : seleccionDefault - 1;
-
-        int y = Console.CursorTop;
-        int cont = 0;
-        int cursorInicio = Console.CursorTop;
-
-        ConsoleKeyInfo accionKey = new ConsoleKeyInfo();
-        do
-        {
-            Console.CursorTop = cursorInicio;
-            cont = 0;
-
-            if (accionKey.Key == ConsoleKey.UpArrow)
-            {
-                seleccionDefault--;
-            }
-            else if (accionKey.Key == ConsoleKey.DownArrow)
-            {
-                seleccionDefault++;
-            }
-
-            if (seleccionDefault > opciones.Length - 1)
-            {
-                seleccionDefault = 0;
-            }
-            else if (seleccionDefault < 0)
-            {
-                seleccionDefault = opciones.Length - 1;
-            }
-
-            Array.ForEach(opciones, opcion =>
-            {
-                if (cont == seleccionDefault)
-                {
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.WriteLine($"-> {opcion}");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    Console.WriteLine("   " + opcion);
-                }
-                cont++;
-            });
-        } while ((accionKey = Console.ReadKey(true)).Key != ConsoleKey.Enter);
-
-        Console.Clear();
-        Console.WriteLine($"Operacion seleccionada: {opciones[seleccionDefault]}");
-        seleccionDefault++;
-        return seleccionDefault++;
     }
 }
+
