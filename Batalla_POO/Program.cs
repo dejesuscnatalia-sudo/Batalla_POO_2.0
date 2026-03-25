a253f3c9cb3f79042d447604be0970feaba77ea5
Markbenalt <gabzuniga07@gmail.com>
2026-03-25 12:46:39 -0700

    Actualizacion del Main 1

--- Batalla_POO/Program.cs
+++ Batalla_POO/Program.cs
@@ -4,10 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
-
-
-
+             Game Jugador = new Game();
+             Jugador.Nombre = null;
+             Jugador.Vida = 100;
+             Jugador.Ataque = 5;
+             Game Enemigo = new Game();
+             Enemigo.Nombre = "CPU 1";
+             Enemigo.Vida = 100;
+             Enemigo.Ataque = 5;
+             do
+             {
+                 Console.WriteLine("Inserte el nombre para su personaje");
+                 Jugador.Nombre = Console.ReadLine();
+             } while (Jugador.Nombre == "");
+             Console.Clear();
             //pantalla de niveles 
             //jugador
             Console.writeline("");
@@ -20,7 +30,7 @@
                     "1-Atacar",
                     "2-Curar",
                     };
-                O = menu.MakeMenu("Seleccione una operacion", Operaciones, 1);
+                O = menu.MakeMenu("Seleccione una accion", Operaciones, 1);
             } while (O < 1 || O > 2);
         }
     }
