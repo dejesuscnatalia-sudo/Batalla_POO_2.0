399e51092f6330a9c1b20583a69ef8a0e63567c5
natha <natha@PATO_PAN>
2026-03-24 21:52:01 -0700

    Agregar archivos de proyecto.

---
+++ Batalla_POO/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Batalla_POO
+{
+    internal class Game
+    {
+        private string nombre;
+        private decimal vida;
+        private decimal ataque;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+}
---
+++ Batalla_POO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Batalla_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
