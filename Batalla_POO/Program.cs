3e15739f8e84e499a23a550813f38a3b0255a75c
nathy_142857 <dejesusc.natalia@gmail.com>
2026-03-25 12:43:18 -0700

    Update Program.cs

--- Batalla_POO/Program.cs
+++ Batalla_POO/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("");
+
+
+
+            //pantalla de niveles 
+            //jugador
+            Console.writeline("");
+            //primera acción: selección de movimiento
+            do
+            {
+                Menu menu = new Menu();
+                string[] Operaciones = new string[]
+                {
+                    "1-Atacar",
+                    "2-Curar",
+                    };
+                O = menu.MakeMenu("Seleccione una operacion", Operaciones, 1);
+            } while (O < 1 || O > 2);
         }
     }
 }
